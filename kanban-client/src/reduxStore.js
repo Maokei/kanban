46e068c6091fdcb1366094bf23714d67b1056566
Rickard Johansson <rickard.joh@gmail.com>
2019-12-21 20:07:22 +0100

    Added redux initial, redux observer,, basic epics

---
+++ kanban-client/src/reduxStore.js
@@ -0,0 +1,34 @@
+import { createStore, applyMiddleware, compose} from "redux";
+import { createEpicMiddleware } from 'redux-observable';
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+import epics from "./epics";
+
+const initialState = {};
+const epicMiddleware = createEpicMiddleware();
+const middleware = [thunk, epicMiddleware];
+
+let store;
+if(window.navigator.userAgent.includes("Chrome")) {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware),
+            window.__REDUX_DEVTOOLS_EXTENSION__ &&
+            window.__REDUX_DEVTOOLS_EXTENSION__()
+        )
+    );
+}else {
+    store = createStore(
+        rootReducer,
+        initialState,
+        compose(
+            applyMiddleware(...middleware)
+        )
+    );
+}
+
+epicMiddleware.run(epics);
+
+export default store;
