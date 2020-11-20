4ba72f319454b621eddaf657a7dd1cc4db436694
maokei <rickard.joh@gmail.com>
2020-11-20 03:23:40 +0100

    delete action, delete reducer, delete gui

--- kanban-client/src/actions/projectActions.js
+++ kanban-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECTS, GET_PROJECT } from "./types";
+import { GET_ERRORS, GET_PROJECTS, GET_PROJECT, DELETE_PROJECT } from "./types";
 
 export const createProject = (project, history) => async dispatch => {
   try {
@@ -37,3 +37,15 @@ export const getProject = (id, history) => async dispatch => {
     history.push("/dashboard");
   }
 };
+
+export const deleteProject = (pi) => async dispatch => {
+  try {
+    const res = await axios.delete(`http://localhost:8080/api/project/${pi}`)
+    dispatch({
+      type: DELETE_PROJECT,
+      payload: pi
+    });
+  }catch(error) {
+    console.log("error delete")
+  }
+}
