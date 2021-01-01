2c7c4c8926747aa15193cf0e5974107d8c80942c
maokei <rickard.joh@gmail.com>
2021-01-01 15:50:11 +0100

    Project test security

--- kanban-server/src/test/java/se/maokei/kanban/ProjectControllerTest.java
+++ kanban-server/src/test/java/se/maokei/kanban/ProjectControllerTest.java
@@ -5,18 +5,19 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import se.maokei.kanban.domain.Project;
 import se.maokei.kanban.repositories.ProjectRepository;
 
 import static org.hamcrest.Matchers.containsString;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -26,6 +27,11 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @RunWith(SpringRunner.class)
 @AutoConfigureMockMvc
 public class ProjectControllerTest {
+    private final String URL_GET_ALL_PROJECTS = "/api/project/all";
+
+    @Autowired
+    private WebApplicationContext context;
+
     @Autowired
     private MockMvc mockMvc;
 
@@ -39,11 +45,17 @@ public class ProjectControllerTest {
         project1.setDescription("This is test project1");
         project1.setProjectIdentifier("MXMX4");
         projectRepository.save(project1);
+
+        /*mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();*/
     }
 
     @Test
+    @WithMockUser(username = "user1", password = "pwd", roles = "USER")
     public void checkForTestProject() throws Exception {
-        mockMvc.perform(get("/api/project/all")
+        mockMvc.perform(get(URL_GET_ALL_PROJECTS)
                 .contentType(MediaType.APPLICATION_JSON)
         ).andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
