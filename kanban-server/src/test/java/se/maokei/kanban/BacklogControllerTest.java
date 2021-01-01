3d5c661d6402f0d136d8e837d787c8ab2c7a9075
maokei <rickard.joh@gmail.com>
2021-01-01 16:25:54 +0100

    CSRF and security for backlog controller test

--- kanban-server/src/test/java/se/maokei/kanban/BacklogControllerTest.java
+++ kanban-server/src/test/java/se/maokei/kanban/BacklogControllerTest.java
@@ -1,6 +1,7 @@
 package se.maokei.kanban;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -8,9 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 import se.maokei.kanban.domain.Backlog;
 import se.maokei.kanban.domain.Project;
 import se.maokei.kanban.domain.ProjectTask;
@@ -24,6 +28,8 @@ import java.util.List;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.Assert.assertEquals;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -32,12 +38,15 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
 @RunWith(SpringRunner.class)
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 public class BacklogControllerTest {
+    private static final String BACKLOG_BASE_URL = "/api/backlog/";
     private static final String updProjectStr = "clown project";
     private static final String updProjectIdentifier = "XXXX5";
     private static final String updSequence = "CLOWN-1";
 
+    @Autowired
+    private WebApplicationContext context;
     @Autowired
     ProjectRepository projectRepository;
     @Autowired
@@ -51,6 +60,11 @@ public class BacklogControllerTest {
 
     @Before
     public void setup() {
+        mockMvc = MockMvcBuilders
+                .webAppContextSetup(context)
+                .apply(springSecurity())
+                .build();
+
         Project project = new Project();
         project.setName(updProjectStr);
         project.setDescription("This is test project");
@@ -72,7 +86,13 @@ public class BacklogControllerTest {
         projectTaskService.addProjectTask(updProjectIdentifier, projectTask);
     }
 
+    @After
+    public void afterAllTests() {
+        projectRepository.deleteAll();
+    }
+
     @Test
+    @WithMockUser(username = "user1", password = "pwd", roles = "USER")
     public void updateProjectTask() throws Exception {
         String newSummary = "This clown task has been updated!";
         Project pro = projectRepository.findByProjectIdentifier(updProjectIdentifier);
@@ -85,14 +105,15 @@ public class BacklogControllerTest {
 
         pt.setSummary(newSummary);
         ObjectMapper objectMapper = new ObjectMapper();
-        mockMvc.perform(patch("/api/backlog/" + backlog.getProjectIdentifier() + "/" + sequence)
+        mockMvc.perform(patch(BACKLOG_BASE_URL + backlog.getProjectIdentifier() + "/" + sequence)
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(objectMapper.writeValueAsString(pt))
+                .with(csrf())
         ).andExpect(status().isOk())
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
 
         //fetch and assert Project Task
-        mockMvc.perform(get("/api/backlog/" + backlog.getProjectIdentifier() + "/" + sequence))
+        mockMvc.perform(get(BACKLOG_BASE_URL + backlog.getProjectIdentifier() + "/" + sequence))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
                 .andExpect(content().string(containsString(newSummary)));
     }
