743a9833cc882aed2e529254b7ba6d78e1c094cb
maokei <rickard.joh@gmail.com>
2021-04-17 23:15:07 +0200

    Removed import

--- kanban-server/src/main/java/se/maokei/kanban/services/ProjectService.java
+++ kanban-server/src/main/java/se/maokei/kanban/services/ProjectService.java
@@ -1,7 +1,6 @@
 package se.maokei.kanban.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import se.maokei.kanban.domain.Backlog;
 import se.maokei.kanban.domain.Project;
